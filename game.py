4d8b53a90d63097f8c763d4ff277647a6bcf1178
cornedev <corneproductions@gmail.com>
2026-01-12 16:58:08 +0100

    Improve collision system and bug fixes.
    Improve collision system, fix wall hopping and glitching through walls. Add a function for creating floors.

--- game.py
+++ game.py
@@ -26,12 +26,16 @@ playervely = 0
 playerjump = -18
 
 floor = pygame.image.load("gfx/floor.png").convert_alpha()
-floorw = 500
-floorh = 100
-floorx = 0
-floory = 600
-floor = pygame.transform.scale(floor, (floorw, floorh))
-floorrect = floor.get_rect(topleft=(floorx, floory))
+def floorcreate(floorx, floory, floorw, floorh):
+    floorimg = pygame.transform.scale(floor, (floorw, floorh))
+    floorrect = floorimg.get_rect(topleft=(floorx, floory))
+    return (floorimg, floorrect)
+
+floors = [
+    floorcreate(400, 400, 1000, 100),
+    floorcreate(200, 500, 1000, 100),
+    floorcreate(200, 600, 1000, 100)
+]
 
 white = (255, 255, 255)
 blue = (135, 206, 235)
@@ -43,27 +47,42 @@ while gamerunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gamerunning = False
-    
+   
+    gamedx = 0
     gameinput = pygame.key.get_pressed()
     if gameinput[pygame.K_a]:
-        playerrect.x -= playerspeed
+        gamedx = -playerspeed
     if gameinput[pygame.K_d]:
-        playerrect.x += playerspeed
+        gamedx = playerspeed
+    playerrect.x += gamedx
+
+    for floorimg, floorrect in floors:
+        if playerrect.colliderect(floorrect):
+            if gamedx > 0:
+                playerrect.right = floorrect.left
+            if gamedx < 0:
+                playerrect.left = floorrect.right
     if gameinput[pygame.K_SPACE] and ground:
         playervely = playerjump
         ground = False
 
     playervely += playergravity
     playerrect.y += playervely
-    if playerrect.colliderect(floorrect):
-        if playervely > 0:
-            playerrect.bottom = floorrect.top
-            playervely = 0
-            ground = True
+    ground = False
+    for floorimg, floorrect in floors:
+        if playerrect.colliderect(floorrect):
+            if playervely > 0:
+                playerrect.bottom = floorrect.top
+                playervely = 0
+                ground = True
+            elif playervely < 0:
+                playerrect.top = floorrect.bottom
+                playervely = 0
 
     gamescreen.fill(blue)
     gamescreen.blit(player, playerrect)
-    gamescreen.blit(floor, floorrect)
+    for floorimg, floorrect in floors:
+        gamescreen.blit(floorimg, floorrect)
 
     pygame.display.flip()
     gameclock.tick(gamefps)
