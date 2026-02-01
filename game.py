affe5f1f61a59c8ee5a5f94b13e82741b2547ea7
cornedev <corneproductions@gmail.com>
2026-02-01 12:27:45 +0100

    Add alot of stuff.
    Added the following things:
    - Spritesheet walking animation support for player
    - Sprites for jumping & crouching
    - Coin creation function
    - Spritesheet coin animation
    - Moved floorcreate & coincreate functions to obstacles.py

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 
+from obstacles import floorcreate, coincreate
+
 pygame.init()
 
 gamew = 1000
@@ -19,34 +21,64 @@ cameray = 0
 worldw = 4000
 worldh = 2000
 
-player = pygame.image.load("gfx/player.png").convert_alpha()
-playerw = 128
+playersheet = pygame.image.load("gfx/playerspritesheet_walk_idle.png").convert_alpha()
+playerframew = 32
+playerframeh = 64
+playerw = 64
 playerh = 128
 playerx = 200
 playery = 200
-player = pygame.transform.scale(player, (playerw, playerh))
+playerframecount = 4
+
+playerframes = []
+for i in range(playerframecount):
+    playerframe = playersheet.subsurface(
+        pygame.Rect(i * playerframew, 0, playerframew, playerframeh)
+    )
+    playerframe = pygame.transform.scale(playerframe, (playerw, playerh))
+    playerframes.append(playerframe)
+
+player = playerframes[0]
 playerrect = player.get_rect(topleft=(playerx, playery))
 playerspeed = 5
 playergravity = 1
 playervely = 0
-playerjump = -18
+playerjumpstrength = -18
+playerframeindex = 0
+playeranimspeed = 0.15
+playercrouch = pygame.image.load("gfx/playerspritesheet_crouch.png").convert_alpha()
+playercrouchframe = pygame.transform.scale(playercrouch, (playerw, playerh))
+playerjump = pygame.image.load("gfx/playerspritesheet_jump.png").convert_alpha()
+playerjumpframe = pygame.transform.scale(playerjump, (playerw, playerh))
 
 floor = pygame.image.load("gfx/floor.png").convert_alpha()
-def floorcreate(floorx, floory, floorw, floorh):
-    floorimg = pygame.transform.scale(floor, (floorw, floorh))
-    floorrect = floorimg.get_rect(topleft=(floorx, floory))
-    return (floorimg, floorrect)
-
 floors = [
-    floorcreate(0, 600, 4000, 100),
-    floorcreate(600, 450, 800, 100),
-    floorcreate(1600, 350, 800, 100),
+    floorcreate(floor, 0, 600, 4000, 100),
+    floorcreate(floor, 600, 450, 800, 100),
+    floorcreate(floor, 1600, 350, 800, 100),
+]
+
+coin = pygame.image.load("gfx/coinspritesheet.png").convert_alpha()
+coinframew = 64
+coinframeh = 64
+coinframecount = 6
+coinanimspeed = 0.15
+
+coinframes = []
+for i in range(coinframecount):
+    coinframe = coin.subsurface(pygame.Rect(i * coinframew, 0, coinframew, coinframeh))
+    coinframe = pygame.transform.scale(coinframe, (64, 64))
+    coinframes.append(coinframe)
+
+coins = [
+    {"rect": coincreate(coinframes[0], 300, 300)[1], "coinframeindex": 0},
 ]
 
 white = (255, 255, 255)
 blue = (135, 206, 235)
 
 ground = True
+playerright = True
 gamerunning = True
 
 while gamerunning:
@@ -56,11 +88,39 @@ while gamerunning:
    
     gamedx = 0
     gameinput = pygame.key.get_pressed()
+    
     if gameinput[pygame.K_a]:
-        gamedx = -playerspeed
+        playerright = False
     if gameinput[pygame.K_d]:
-        gamedx = playerspeed
+        playerright = True
+
+    if not (gameinput[pygame.K_s] and ground):
+        if gameinput[pygame.K_a]:
+            gamedx = -playerspeed
+        if gameinput[pygame.K_d]:
+            gamedx = playerspeed
     playerrect.x += gamedx
+    
+    if not ground:
+        player = playerjumpframe
+        if not playerright:
+            player = pygame.transform.flip(player, True, False)
+    elif gameinput[pygame.K_s] and ground:
+        player = playercrouchframe
+        if not playerright:
+            player = pygame.transform.flip(player, True, False)
+    elif gamedx != 0:
+        playerframeindex += playeranimspeed
+        if playerframeindex >= len(playerframes):
+            playerframeindex = 0
+        player = playerframes[int(playerframeindex)]
+        if not playerright:
+            player = pygame.transform.flip(player, True, False)
+    else:
+        playerframeindex = 0
+        player = playerframes[0]
+        if not playerright:
+            player = pygame.transform.flip(player, True, False)
 
     for floorimg, floorrect in floors:
         if playerrect.colliderect(floorrect):
@@ -69,7 +129,7 @@ while gamerunning:
             if gamedx < 0:
                 playerrect.left = floorrect.right
     if gameinput[pygame.K_SPACE] and ground:
-        playervely = playerjump
+        playervely = playerjumpstrength
         ground = False
 
     playervely += playergravity
@@ -90,9 +150,18 @@ while gamerunning:
 
     gamescreen.fill(blue)
     gamescreen.blit(player, (playerrect.x - camerax, playerrect.y - cameray))
+
+    coins = [coin for coin in coins if not playerrect.colliderect(coin["rect"])]
+    for coin in coins:
+        coin["coinframeindex"] += coinanimspeed
+        if coin["coinframeindex"] >= len(coinframes):
+            coin["coinframeindex"] = 0
+
+        coincurrentframe = coinframes[int(coin["coinframeindex"])]
+        gamescreen.blit(coincurrentframe, (coin["rect"].x - camerax, coin["rect"].y - cameray))
+
     for floorimg, floorrect in floors:
         gamescreen.blit(floorimg, (floorrect.x - camerax, floorrect.y - cameray))
-
     pygame.display.flip()
     gameclock.tick(gamefps)
 
