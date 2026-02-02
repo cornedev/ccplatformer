c1b937a4ea26d448e9215435bb4b4cc5856690d5
cornedev <corneproductions@gmail.com>
2026-02-02 19:38:54 +0100

    Add new sfx functions.

--- audio.py
+++ audio.py
@@ -5,4 +5,15 @@ def musicstart():
     pygame.mixer.music.set_volume(0.5)
     pygame.mixer.music.play(-1)
 
+audiojump = pygame.mixer.Sound("sfx/jump.mp3")
+audiojump.set_volume(0.5)
+audiocoin = pygame.mixer.Sound("sfx/coin.mp3")
+audiocoin.set_volume(0.5)
+
+def playerjumpsfx():
+    audiojump.play()
+
+def playercoinsfx():
+    audiocoin.play()
+
 musicrunning = True
